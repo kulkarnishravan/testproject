896628722e85fd96d9c43e151576d051f3581e56
Kulkarni <Shravan.Kulkarni@unisys.com>
2020-07-29 22:29:01 +0530

    Updates

---
+++ precache-manifest.215db456fe228dd91301b24215442b2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d0390794e711dd780766c65f5eeabbc",
+    "url": "/testproject/index.html"
+  },
+  {
+    "revision": "fd783654b6838e822dc5",
+    "url": "/testproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "de89d8fad7efeeabc48b",
+    "url": "/testproject/static/js/2.43755bb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testproject/static/js/2.43755bb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd783654b6838e822dc5",
+    "url": "/testproject/static/js/main.635f67a6.chunk.js"
+  },
+  {
+    "revision": "ce241093dfc08de96bda",
+    "url": "/testproject/static/js/runtime-main.4b9fd284.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
